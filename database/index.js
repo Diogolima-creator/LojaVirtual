3138ef8a616bec6f5043bdc94e5cc172722a1a2b
Diogolima-creator <62246037+Diogolima-creator@users.noreply.github.com>
2022-02-14 08:34:53 -0400

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://loja-virtual-bice.vercel.app/lojavirtual')
+mongoose.connect('mongodb://localhost/lojavirtual')
 
 mongoose.Promise = global.Promise;
 
